c1989dcc0f37cad8c40ac3da0ae20be4f9775541
Zach Orr <zachorr139@gmail.com>
2012-09-13 13:09:56 -0400

    Took out hard-codings and set up for landscape support

--- SampleProject/Example/FDStatusBarNotifierView.m
+++ SampleProject/Example/FDStatusBarNotifierView.m
@@ -16,24 +16,19 @@
 
 @implementation FDStatusBarNotifierView
 
-#if (UI_USER_INTERFACE_IDIOM == UIUserInterfaceIdiomPad)
-    #define kNotifierViewInitialFramePortrait   CGRectMake(0, 20, 768, 20)
-    #define kNotifierViewFinalFramePortrait     CGRectMake(0, 0, 768, 20)
-    #define kMessageLabelInitialFramePortrait   CGRectMake(10, 0, 748, 20)
-    #define kMessageLabelInitialFrameLandscape  CGRectMake(10, 0, 1004, 20)
-#else
-    #define kNotifierViewInitialFramePortrait   CGRectMake(0, 20, 320, 20)
-    #define kNotifierViewFinalFramePortrait     CGRectMake(0, 0, 320, 20)
-    #define kMessageLabelInitialFramePortrait   CGRectMake(10, 0, 300, 20)
-    #define kMessageLabelInitialFrameLandscape  CGRectMake(10, 0, 460, 20)
-#endif
-
 - (id)init {
     self = [super init];
     if (self) {
-        self.frame = kNotifierViewInitialFramePortrait;
-        
-        self.messageLabel = [[UILabel alloc] initWithFrame:kMessageLabelInitialFramePortrait];
+        if(UIDeviceOrientationIsPortrait([[UIDevice currentDevice] orientation]) || [[UIDevice currentDevice] orientation] == UIDeviceOrientationUnknown) {
+            NSLog(@"Portrait");
+            self.frame = CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.width, 20);
+            self.messageLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, ([UIScreen mainScreen].bounds.size.width - 20), 20)];
+        }
+        else {
+            NSLog(@"Landscape");
+            self.frame = CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.height, 20);
+            self.messageLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, ([UIScreen mainScreen].bounds.size.height - 20), 20)];
+        }
         self.messageLabel.textColor = [UIColor whiteColor];
         self.messageLabel.backgroundColor = [UIColor blackColor];
         self.messageLabel.textAlignment = UITextAlignmentCenter;
@@ -49,11 +44,19 @@
 - (id)initWithMessage:(NSString *)message {
     self = [super init];
     if (self) {
-        self.frame = kNotifierViewInitialFramePortrait;
+        if(UIDeviceOrientationIsPortrait([[UIDevice currentDevice] orientation]) || [[UIDevice currentDevice] orientation] == UIDeviceOrientationUnknown) {
+            NSLog(@"Portrait");
+            self.frame = CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.width, 20);
+            self.messageLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, ([UIScreen mainScreen].bounds.size.width - 20), 20)];
+        }
+        else {
+            NSLog(@"Landscape");
+            self.frame = CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.height, 20);
+            self.messageLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, ([UIScreen mainScreen].bounds.size.height - 20), 20)];
+        }
         self.message = message;
         self.backgroundColor = [UIColor blackColor];
         
-        self.messageLabel = [[UILabel alloc] initWithFrame:kMessageLabelInitialFramePortrait];
         self.messageLabel.textColor = [UIColor whiteColor];
         self.messageLabel.text = message;
         self.messageLabel.backgroundColor = [UIColor blackColor];
@@ -71,19 +74,26 @@
 - (id)initWithMessage:(NSString *)message delegate:(id /*<StatusBarNotifierViewDelegate>*/)delegate {
     self = [super init];
     if (self) {
-        self.frame = kNotifierViewInitialFramePortrait;
+        if(UIDeviceOrientationIsPortrait([[UIDevice currentDevice] orientation]) || [[UIDevice currentDevice] orientation] == UIDeviceOrientationUnknown) {
+            NSLog(@"Portrait");
+            self.frame = CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.width, 20);
+            self.messageLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, ([UIScreen mainScreen].bounds.size.width - 20), 20)];
+        }
+        else {
+            NSLog(@"Landscape");
+            self.frame = CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.height, 20);
+            self.messageLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, ([UIScreen mainScreen].bounds.size.height - 20), 20)];
+        }
         self.delegate = delegate;
         self.message = message;
         self.backgroundColor = [UIColor blackColor];
         
-        self.messageLabel = [[UILabel alloc] initWithFrame:kMessageLabelInitialFramePortrait];
         self.messageLabel.textColor = [UIColor whiteColor];
         self.messageLabel.text = message;
         self.messageLabel.backgroundColor = [UIColor blackColor];
         self.messageLabel.textAlignment = UITextAlignmentCenter;
         self.messageLabel.font = [UIFont boldSystemFontOfSize:12];
         [self addSubview:self.messageLabel];
-        
         self.timeOnScreen = 2.0;
     }
     return self;
@@ -96,35 +106,64 @@
     [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationSlide];
     [window insertSubview:self atIndex:0];
     
-    [UIView animateWithDuration:.4 animations:^{
-        self.frame = kNotifierViewFinalFramePortrait;
-    } completion:^(BOOL finished){
-        
-        if (self.delegate && [self.delegate respondsToSelector:@selector(didPresentNotifierView:)])
-            [self.delegate didPresentNotifierView:self];
-        
-        [NSTimer scheduledTimerWithTimeInterval:self.timeOnScreen target:self selector:@selector(hide) userInfo:nil repeats:NO];
-        
-    }];
+    if([[UIDevice currentDevice] orientation] == UIDeviceOrientationPortrait) {
+        [UIView animateWithDuration:.4 animations:^{
+            self.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 20);
+        } completion:^(BOOL finished){
+            
+            if (self.delegate && [self.delegate respondsToSelector:@selector(didPresentNotifierView:)])
+                [self.delegate didPresentNotifierView:self];
+            
+            [NSTimer scheduledTimerWithTimeInterval:self.timeOnScreen target:self selector:@selector(hide) userInfo:nil repeats:NO];
+            
+        }];
+    }
+    else {
+        [UIView animateWithDuration:.4 animations:^{
+            self.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.height, 20);
+        } completion:^(BOOL finished){
+            
+            if (self.delegate && [self.delegate respondsToSelector:@selector(didPresentNotifierView:)])
+                [self.delegate didPresentNotifierView:self];
+            
+            [NSTimer scheduledTimerWithTimeInterval:self.timeOnScreen target:self selector:@selector(hide) userInfo:nil repeats:NO];
+            
+        }];
+    }
 }
 
 - (void)hide {
     
     if (self.delegate && [self.delegate respondsToSelector:@selector(willHideNotifierView:)])
         [self.delegate willHideNotifierView:self];
-    
-    [UIView animateWithDuration:.4 animations:^{
-        self.frame = kNotifierViewInitialFramePortrait;
-        [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationSlide];
-    } completion:^(BOOL finished){
-        if (finished) {
-            
-            if (self.delegate && [self.delegate respondsToSelector:@selector(didHideNotifierView:)])
-                [self.delegate didHideNotifierView:self];
-            
-            [self removeFromSuperview];
-        }
-    }];
+    if([[UIDevice currentDevice] orientation] == UIDeviceOrientationPortrait) {
+        [UIView animateWithDuration:.4 animations:^{
+            self.frame = CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.width, 20);
+            [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationSlide];
+        } completion:^(BOOL finished){
+            if (finished) {
+                
+                if (self.delegate && [self.delegate respondsToSelector:@selector(didHideNotifierView:)])
+                    [self.delegate didHideNotifierView:self];
+                
+                [self removeFromSuperview];
+            }
+        }];
+    }
+    else {
+        [UIView animateWithDuration:.4 animations:^{
+            self.frame = CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.height, 20);
+            [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationSlide];
+        } completion:^(BOOL finished){
+            if (finished) {
+                
+                if (self.delegate && [self.delegate respondsToSelector:@selector(didHideNotifierView:)])
+                    [self.delegate didHideNotifierView:self];
+                
+                [self removeFromSuperview];
+            }
+        }];
+    }
 }
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
