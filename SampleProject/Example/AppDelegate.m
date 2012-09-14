2dcfcd837fbbbc62c062d2a72d1377ada2cc6562
Francesco Di Lorenzo <fradilorenzo92@gmail.com>
2012-09-14 10:18:00 +0200

    Now the project runs fine on the iPhone simulator

--- SampleProject/Example/AppDelegate.m
+++ SampleProject/Example/AppDelegate.m
@@ -16,10 +16,17 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    if ([[[UIDevice currentDevice] model] isEqualToString:@"iPhone"] || [[[UIDevice currentDevice] model] isEqualToString:@"iPod touch"])
+    
+    NSString *currentDeviceModel = [[UIDevice currentDevice] model];
+    
+    // Necessary, to make this work also on the simulator
+    BOOL iPhoneORiPod = ([currentDeviceModel rangeOfString:@"iPhone"].location != NSNotFound || [currentDeviceModel rangeOfString:@"iPod touch"].location != NSNotFound);
+    
+    if (iPhoneORiPod)
         self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
     else
         self.viewController = [[ViewController alloc] initWithNibName:@"ViewController_iPad" bundle:nil];
+                                                                                   
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     return YES;
